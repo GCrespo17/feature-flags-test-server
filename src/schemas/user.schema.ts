de1c205b3e9bc1e6b571f3198755961c9c1d9eed
Gabriel <gcrespo1705@gmail.com>
2025-07-10 19:53:50 -0400

    Modificacion del schema para los servicios de login

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -5,10 +5,13 @@ export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   username!: string;
 
   @Prop({ required: true })
+  password!: string;
+
+  @Prop({ required: true, unique: true })
   email!: string;
 
   @Prop({ default: 'user' })
